e5e32a1cb962074210f687be00d146d568806859
Michael Liendo <model3volution@gmail.com>
2018-08-06 14:43:24 -0500

    feat: enable consumer to use own meetup name

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const meetupInfoController = require('./controllers/meetup').meetupInfo
 
-const {PORT} = process.env
 const app = express()
 
 app.use(bodyParser.urlencoded({extended:true}))
